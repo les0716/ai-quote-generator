02c0166fca8abaccbbef921eaf4c6111a0957679
les0716 <141790794+les0716@users.noreply.github.com>
2024-03-17 13:24:31 -0700

    integrated AI in JS

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-function generateQuote(event) {
-  event.preventDefault();
+function displayQuote(response) {
   new Typewriter("#quote-container", {
-    strings:
-      "The greatest glory in living lies not in never falling, but in rising every time we fall.-Nelson Mandelau",
+    strings: response.data.answer,
     autoStart: true,
     delay: 0.1,
     cursor: "",
   });
+}
+
+function generateQuote(event) {
+  event.preventDefault();
+  let submitInput = document.querySelector("#submit-input");
+  let prompt = `user instrugtions: generate a quote about ${submitInput.value}`;
+  let context =
+    "you are a talented motivational quote generator. Please generate short quotes in basic HTML. Make sure to follo users instructions. sign with SheCodes AI at the end of the quote in <strong> element ";
+  let apiKey = "a54e0e42t68cb050731e3eo2f4afcbca";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiURL).then(displayQuote);
+  console.log("generating quote");
 }
 
 let quoteFormElement = document.querySelector("#quote-content");
